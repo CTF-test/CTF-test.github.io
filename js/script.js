95cc4c25d5ea303dc6488210198b7e7af12a5d1c
Lucas <he201669@students.ephec.be>
2021-05-26 17:32:23 +0200

    premier essai

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function vérificationOeil(){
+	var reponse = document.getElementById("oeil").value;
+	if (reponse == "trouite123"){
+		alert("C'est le bon flag");
+	}
+	else{
+		alert("Ce n'est pas le bon flag");
+	}
+}
