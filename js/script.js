e4e5ccca0628a6761a009ba1d9637c3654bf9e52
Lucas <he201669@students.ephec.be>
2021-05-26 18:30:05 +0200

    update crack the rar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,36 @@
-function vérificationOeil(){
+function verificationOeil(){
 	var reponse = document.getElementById("oeil").value;
 	if (reponse == "trouite123"){
+		alert("C'est le bon flag");
+	}
+	else{
+		alert("Ce n'est pas le bon flag");
+	}
+}
+
+function verificationRar1(){
+	var reponse = document.getElementById("rar1").value;
+	if (reponse == "A5ea6dg578BGF"){
+		alert("C'est le bon flag");
+	}
+	else{
+		alert("Ce n'est pas le bon flag");
+	}
+}
+
+function verificationRar2(){
+	var reponse = document.getElementById("rar2").value;
+	if (reponse == "Gdqaze5186azd31q0"){
+		alert("C'est le bon flag");
+	}
+	else{
+		alert("Ce n'est pas le bon flag");
+	}
+}
+
+function verificationRar3(){
+	var reponse = document.getElementById("rar3").value;
+	if (reponse == "kellydeeAussi"){
 		alert("C'est le bon flag");
 	}
 	else{
